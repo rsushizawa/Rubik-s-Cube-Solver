212b260ab23daf08f74a1c109379609226468e2c
rsushizawa <rodrigo.syu@puccampinas.edu.br>
2026-09-24 10:31:58 -0300

    feat: windows compatible

--- include/State.hpp
+++ include/State.hpp
@@ -20,10 +20,12 @@ inline bool operator!=(const State &a, const State &b) {
   return a.full_state != b.full_state;
 }
 
-template <> struct std::hash<State> {
-  std::size_t operator()(const State &s) const {
-    return std::hash<uint64_t>()(s.full_state);
+namespace std {
+template <> struct hash<State> {
+  size_t operator()(const State &s) const {
+    return hash<uint64_t>()(s.full_state);
   }
 };
+} // namespace std
 
 inline constexpr State SOLVED_STATE{0x1C1814100C080400ULL};
